e8b17e5d045982aba44c780854db2f19ba970098
Frederick Polgardy <fred@polgardy.com>
2017-01-29 20:44:51 -0700

    stylistic

--- rules.ex
+++ rules.ex
@@ -3,17 +3,17 @@ defmodule Checkers do
     @size 8
     @range 0 .. @size - 1
 
-    def initial() do
+    def initial do
       {
-        { +1,  0, +1,  0, +1,  0, +1,  0 },
-        {  0, +1,  0, +1,  0, +1,  0, +1 },
-        { +1,  0, +1,  0, +1,  0, +1,  0 },
-        {  0,  0,  0,  0,  0,  0,  0,  0 },
-        {  0,  0,  0,  0,  0,  0,  0,  0 },
-        {  0, -1,  0, -1,  0, -1,  0, -1 },
-        { -1,  0, -1,  0, -1,  0, -1,  0 },
-        {  0, -1,  0, -1,  0, -1,  0, -1 }
-      }
+        {+1,  0, +1,  0, +1,  0, +1,  0},
+        { 0, +1,  0, +1,  0, +1,  0, +1},
+        {+1,  0, +1,  0, +1,  0, +1,  0},
+        { 0,  0,  0,  0,  0,  0,  0,  0},
+        { 0,  0,  0,  0,  0,  0,  0,  0},
+        { 0, -1,  0, -1,  0, -1,  0, -1},
+        {-1,  0, -1,  0, -1,  0, -1,  0},
+        { 0, -1,  0, -1,  0, -1,  0, -1}
+     }
     end
 
     def get_p(b, x, y), do: b |> elem(y) |> elem(x)
@@ -32,11 +32,14 @@ defmodule Checkers do
 
     def playable?(x, y), do: rem(x + y, 2) == 0
 
-    def in_bounds?(x, y, nx, ny), do: (x in @range) && (y in @range) && (nx in @range) && (ny in @range)
+    def in_bounds?(x, y, nx, ny),
+      do: (x in @range) && (y in @range) && (nx in @range) && (ny in @range)
 
-    def jump?(x, y, nx, ny), do: (abs(nx - x) == 2) && (abs(ny - y) == 2)
+    def jump?(x, y, nx, ny),
+      do: (abs(nx - x) == 2) && (abs(ny - y) == 2)
 
-    def promoted?(_nx, ny, p), do: (p == +1 && ny == @size - 1) || (p == -1 && ny == 0)
+    def promoted?(_nx, ny, p),
+      do: (p == +1 && ny == @size - 1) || (p == -1 && ny == 0)
 
     def promote(nx, ny, p) do
       if promoted?(nx, ny, p), do: 2 * p, else: p
@@ -45,80 +48,85 @@ defmodule Checkers do
     def squares(b) do
       for x <- @range,
           y <- @range,
-          playable?(x, y), do: { x, y, get_p(b, x, y) }
+          playable?(x, y), do: {x, y, get_p(b, x, y)}
     end
 
     def my_squares(b, s) do
-      for { x, y, p } <- squares(b), mine?(s, p), do: { x, y, p }
+      for {x, y, p} <- squares(b), mine?(s, p), do: {x, y, p}
     end
 
     def directions(p) do
       case p do
-        +1 -> [{ +1, +1 }, { -1, +1 }]
-        +2 -> [{ +1, +1 }, { -1, +1 }, { +1, -1 }, { -1, -1 }]
-        -1 -> [{ -1, -1 }, { +1, -1 }]
-        -2 -> [{ -1, -1 }, { +1, -1 }, { -1, +1 }, { +1, +1 }]
+        +1 -> [{+1, +1}, {-1, +1}]
+        +2 -> [{+1, +1}, {-1, +1}, {+1, -1}, {-1, -1}]
+        -1 -> [{-1, -1}, {+1, -1}]
+        -2 -> [{-1, -1}, {+1, -1}, {-1, +1}, {+1, +1}]
       end
     end
 
     def do_jump(b, s, x, y, nx, ny) do
       if in_bounds?(x, y, nx, ny) && jump?(x, y, nx, ny) do
-        mx = div(x + nx, 2)
-        my = div(y + ny, 2)
-
-        if opp?(s, get_p(b, mx, my)) && open?(get_p(b, nx, ny)) do
-          b |> set_p(x, y, 0)
+        with mx = div(x + nx, 2),
+             my = div(y + ny, 2) do
+          if opp?(s, get_p(b, mx, my)) && open?(get_p(b, nx, ny)) do
+            b
+            |> set_p(x, y, 0)
             |> set_p(mx, my, 0)
             |> set_p(nx, ny, promote(nx, ny, get_p(b, x, y)))
+          end
         end
       end
     end
 
     def collect_jumps(b, s, x, y, p) do
-      for { dx, dy } <- directions(p),
+      for {dx, dy} <- directions(p),
           nx = x + 2 * dx,
           ny = y + 2 * dy,
           nb = do_jump(b, s, x, y, nx, ny),
-          jumps = collect_jumps(nb, s, nx, ny, p), do: [ { nx, ny } | jumps ]
+          jumps = collect_jumps(nb, s, nx, ny, p), do: [{nx, ny} | jumps]
     end
 
     def jumps_from(b, s, x, y) do
-      jumps = collect_jumps(b, s, x, y, get_p(b, x, y))
-      if length(jumps) > 0, do: [ { x, y } | jumps ]
+      with jumps = collect_jumps(b, s, x, y, get_p(b, x, y)) do
+        if length(jumps) > 0, do: [{x, y} | jumps]
+      end
     end
 
     def my_jumps(b, s) do
-      for { x, y, _ } <- my_squares(b, s),
+      for {x, y, _} <- my_squares(b, s),
           jumps = jumps_from(b, s, x, y), do: jumps
     end
 
     def do_move(b, _s, x, y, nx, ny) do
       if in_bounds?(x, y, nx, ny) && open?(get_p(b, nx, ny)) do
-        b |> set_p(x, y, 0)
-          |> set_p(nx, ny, promote(nx, ny, get_p(b, x, y)))
+        b
+        |> set_p(x, y, 0)
+        |> set_p(nx, ny, promote(nx, ny, get_p(b, x, y)))
       end
     end
 
     def collect_moves(b, s, x, y, p) do
-      for { dx, dy } <- directions(p),
+      for {dx, dy} <- directions(p),
           nx = x + dx,
           ny = y + dy,
-          do_move(b, s, x, y, nx, ny), do: [ { nx, ny } ]
+          do_move(b, s, x, y, nx, ny), do: [{nx, ny}]
     end
 
     def moves_from(b, s, x, y) do
-      moves = collect_moves(b, s, x, y, get_p(b, x, y))
-      if length(moves) > 0, do: [ { x, y } | moves ]
+      with moves = collect_moves(b, s, x, y, get_p(b, x, y)) do
+        if length(moves) > 0, do: [{x, y} | moves]
+      end
     end
 
     def my_moves(b, s) do
-      for { x, y, _ } <- my_squares(b, s),
+      for {x, y, _} <- my_squares(b, s),
           moves = moves_from(b, s, x, y), do: moves
     end
 
     def my_plays(b, s) do
-      jumps = my_jumps(b, s)
-      if length(jumps) > 0, do: jumps, else: my_moves(b, s)
+      with jumps = my_jumps(b, s) do
+        if length(jumps) > 0, do: jumps, else: my_moves(b, s)
+      end
     end
   end
 end
